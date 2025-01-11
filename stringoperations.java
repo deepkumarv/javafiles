907260e87c6e649fb79a9dfdb528667217a0aff7
V PRADEEP KUMAR <58266287+deepkumarv@users.noreply.github.com>
2025-01-11 13:45:56 +0530

    Add files via upload

---
+++ stringoperations.java
@@ -0,0 +1,16 @@
+package test_java;
+
+public class stringoperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String a = "pradeep";
+		System.out.println(a);
+		StringBuilder sb = new StringBuilder(a);
+		sb.reverse();
+		System.out.println(sb);
+
+	}
+
+}
---
+++ test_prime.java
@@ -0,0 +1,13 @@
+package test_java;
+
+public class test_prime {
+	
+	int i = 0;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("It is my first java program");
+
+	}
+
+}
